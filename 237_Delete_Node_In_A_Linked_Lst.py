48fcea27ea07a84460bc640a7090f7460a6826c6
vazha <rohithv63@gmail.com>
2020-06-23 12:37:21 +0530

    Leetcode 237 Problem

---
+++ 237_Delete_Node_In_A_Linked_Lst.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        
+        while 1:
+            node.val = node.next.val
+            
+            if node.next.next == None:
+                node.next = None
+                break;
+                
+            node = node.next
