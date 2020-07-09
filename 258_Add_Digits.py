4ae5d5ae5baeffcc291cc0d456468899d5a8bd80
vazha <rohithv63@gmail.com>
2020-07-09 19:53:36 +0530

    Leetcode 258 Problem

---
+++ 258_Add_Digits.py
@@ -0,0 +1,8 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num < 10:
+            return num
+        if num % 9 ==0:
+            return 9
+        return num % 9
+        
