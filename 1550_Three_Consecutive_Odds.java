44aee289fb5987f965ef87ab4f07ca02a1799db4
rohithv07 <rohithv63@gmail.com>
2020-08-16 12:13:59 +0530

    Leetcode 1550 Problem Java

---
+++ 1550_Three_Consecutive_Odds.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        for (int i=0; i+2<arr.length; i++){
+            if (arr[i]%2 != 0 && arr[i+1]%2 != 0 && arr[i+2]%2 != 0)
+                return true;
+        }
+        return false;
+    }
+}
