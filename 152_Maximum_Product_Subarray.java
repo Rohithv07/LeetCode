1218ceee9f2af28772fb2cc26560665bd811266a
rohithv07 <rohithv63@gmail.com>
2020-09-11 16:50:05 +0530

    Leetcode Problem 152 added another solution 0ms Java

--- 152_Maximum_Product_Subarray.java
+++ 152_Maximum_Product_Subarray.java
@@ -17,3 +17,27 @@ class Solution {
         return max;
     }
 }
+
+
+Another Solution:
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        int product = 1;
+        int maximumProduct = Integer.MIN_VALUE;
+        for (int i=0; i<nums.length; i++) {
+            product *= nums[i];
+            maximumProduct = Math.max(maximumProduct, product);
+            if (product == 0)
+                product = 1;
+        }
+        product = 1;
+        for (int i=nums.length-1; i>=0; i--) {
+            product *= nums[i];
+            maximumProduct = Math.max(maximumProduct, product);
+            if (product == 0)
+                product = 1;
+        }
+        return maximumProduct;
+    }
+}
