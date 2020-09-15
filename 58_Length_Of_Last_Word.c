89c09ae46cbf2e4f8359539ce210d23654e1d98b
rohithv07 <rohithv63@gmail.com>
2020-09-15 16:57:10 +0530

    Leetcode 58 Problem C

---
+++ 58_Length_Of_Last_Word.c
@@ -0,0 +1,18 @@
+
+
+int lengthOfLastWord(char * s){
+    int i;
+    int count = 0;
+    
+    for(i = strlen(s)-1; i >= 0; i--){
+        if(s[i] == ' '){
+            if(count != 0)
+                return count;
+            count = 0;
+        }
+        else
+            count ++;
+           
+    }
+    return count;
+}
