db27b8cabbf4c3ccbe926e2919500b4873d7097f
rohithv07 <rohithv63@gmail.com>
2020-09-29 00:32:50 +0530

    Leetcode 1551 Problem Java

---
+++ 1551_Min_Operation_To_Make_Array_Equal.java
@@ -0,0 +1,6 @@
+// Refer: https://leetcode.com/problems/minimum-operations-to-make-array-equal/discuss/794163/Java-2-Lines
+class Solution {
+    public int minOperations(int n) {
+        return n * n / 4;
+    }
+}
