a8fc0809a2d25a4f762d3d1c1f818400480b6c57
rohithv07 <rohithv63@gmail.com>
2020-10-04 19:24:46 +0530

    Leetcode 1603 Problem Java

--- 1603_Design_Parking_System.java
+++ 1603_Design_Parking_System.java
@@ -36,3 +36,22 @@ class ParkingSystem {
  * ParkingSystem obj = new ParkingSystem(big, medium, small);
  * boolean param_1 = obj.addCar(carType);
  */
+
+
+Solution 2:
+class ParkingSystem {
+    int [] count;
+    public ParkingSystem(int big, int medium, int small) {
+        count = new int[] {big, medium, small};
+    }
+    
+    public boolean addCar(int carType) {
+        return count[carType-1] -- > 0;
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
+ */
