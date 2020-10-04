0fe093d568362d2be64bd17d7f3e13bb0a5f135c
rohithv07 <rohithv63@gmail.com>
2020-10-04 19:21:18 +0530

    Leetcode 1603 Problem Java

---
+++ 1603_Design_Parking_System.java
@@ -0,0 +1,38 @@
+class ParkingSystem {
+    private List<Integer> s;
+    private List<Integer> m;
+    private List<Integer> b;
+    private int big;
+    private int small;
+    private int medium;
+    public ParkingSystem(int big, int medium, int small) {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+        s = new ArrayList<>();
+        m = new ArrayList<>();
+        b = new ArrayList<>();;
+    }
+    
+    public boolean addCar(int carType) {
+        if (carType == 1 && b.size() < big) {
+            b.add(1);
+            return true;
+        }
+        if (carType == 2 && m.size() < medium) {
+            m.add(1);
+            return true;
+        }
+        if (carType == 3 && s.size() < small) {
+            s.add(1);
+            return true;
+        }
+        return false;
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
+ */
