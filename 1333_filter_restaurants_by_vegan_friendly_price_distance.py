172729efef8e95be315c1df7ca34d2af688ab8dd
rohithv07 <rohithv63@gmail.com>
2020-10-12 11:26:39 +0530

    Leetcode 1333 Problem Java

---
+++ 1333_filter_restaurants_by_vegan_friendly_price_distance.py
@@ -0,0 +1,5 @@
+# sort the restaurants and add the filters
+class Solution:
+    def filterRestaurants(self, restaurants: List[List[int]], veganFriendly: int, maxPrice: int, maxDistance: int) -> List[int]:
+        restaurants.sort(key=lambda x : (-x[1], -x[0]))
+        return [i for i, r, v, p, d in restaurants if v>=veganFriendly and p<=maxPrice and d<=maxDistance]
