9a671e0e35372ea24c0bfe48ca460833c1fd6ecd
rohithv07 <rohithv63@gmail.com>
2020-10-16 17:13:31 +0530

    Leetcode 74 Problem Java

---
+++ 74_Search_a_2d_matrix.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        // lets start by brute force
+        if (matrix.length == 0)
+            return false;
+        int row = matrix.length; 
+        int col = matrix[0].length;
+        // since it is sorted the maximum will be in the last row last col and the min element will be in the first row first col
+        if (target > matrix[row-1][col-1] || target < matrix[0][0])
+            return false;
+        // O(mn)
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                if (matrix[i][j] == target)
+                    return true;
+            }
+        }
+        return false;
+        
+        
+        // since its searching in a matrix, we can make use of binary search.
+        if (matrix.length == 0)
+            return false;
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int left = 0;
+        int right = row * col - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (matrix[mid/col][mid%col] == target)
+                return true;
+            else if (matrix[mid/col][mid%col] < target)
+                left = mid + 1;
+            else 
+                right = mid - 1;
+        }
+        return false;
+        // O(log(row) + log(col))
+    }
+}
+
