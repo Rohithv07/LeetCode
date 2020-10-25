236addf75038f1e5f7c0e1c1877ef882593516b3
rohithv07 <rohithv63@gmail.com>
2020-10-25 17:53:59 +0530

    Leetcode 1137 Problem Java

---
+++ 1137_Nth_Tribonacci_Number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int tribonacci(int n) {
+         if (n < 2)
+            return n;
+        int [] dp = new int [n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i=3; i<=n; i++)
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        return dp[n];
+    }
+}
