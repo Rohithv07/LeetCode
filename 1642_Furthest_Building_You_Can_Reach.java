abc3704e77cb90ca3020dd8c8ebbb6b6449aa159
rohithv07 <rohithv63@gmail.com>
2020-11-06 17:49:46 +0530

    Leetcode 1642 Problem Java Updated

--- 1642_Furthest_Building_You_Can_Reach.java
+++ 1642_Furthest_Building_You_Can_Reach.java
@@ -31,3 +31,22 @@ class Solution {
         return reachableBuilding;
     }
 }
+// wronng answer
+
+class Solution {
+    public int furthestBuilding(int[] A, int bricks, int ladders) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < A.length - 1; i++) {
+            int d = A[i + 1] - A[i];
+            if (d > 0)
+                pq.add(d);
+            if (pq.size() > ladders)
+                bricks -= pq.poll();
+            if (bricks < 0)
+                return i;
+        }
+        return A.length - 1;
+    
+    }
+}
+// correct answer
