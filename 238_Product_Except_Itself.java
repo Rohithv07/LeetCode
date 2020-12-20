41ed83b4a890f427a79a09560b98c204c62bdc33
rohithv07 <rohithv63@gmail.com>
2020-12-20 12:10:43 +0530

    Leetcode 238 problem java

--- 238_Product_Except_Itself.java
+++ 238_Product_Except_Itself.java
@@ -27,3 +27,35 @@ class Solution {
         return outputArray;
     }
 }
+
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        /*
+        This is second possible method without using division
+        int [] left = new int [nums.length];
+        int [] right = new int [nums.length];
+        int [] answer = new int [nums.length];
+        left[0] = 1;
+        for (int i=1; i<nums.length; i++) 
+            left[i] = nums[i-1] * left[i-1];
+        right[nums.length - 1] = 1;
+        for (int i=nums.length - 2; i>=0; i--)
+            right[i] = nums[i+1] * right[i+1];
+        for (int i=0; i<nums.length; i++)
+            answer[i] = left[i] * right[i];
+        return answer;
+        */
+        // 3rd method without division
+        int [] answer = new int[nums.length];
+        answer[0] = 1;
+        for (int i=1; i<nums.length; i++)
+            answer[i] = answer[i-1] * nums[i-1];
+        int right = 1;
+        for (int i = nums.length-1; i>=0; i--) {
+            answer[i] = answer[i] * right;
+            right *= nums[i];
+        }
+        return answer;
+    }
+}
