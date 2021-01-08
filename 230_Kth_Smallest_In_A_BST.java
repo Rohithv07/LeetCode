2122a747866db24b59107ace39cce0391cedfe6c
rohithv07 <rohithv63@gmail.com>
2021-01-08 23:10:39 +0530

    leetcode 230 problem java

--- 230_Kth_Smallest_In_A_BST.java
+++ 230_Kth_Smallest_In_A_BST.java
@@ -1,3 +1,4 @@
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -30,4 +31,37 @@ class Solution {
         }
         return result;
     }
+}
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        ArrayList<Integer> result =  inOrderTraversal(root, new ArrayList<Integer>());
+        return result.get(k - 1);
+    }
+     public ArrayList<Integer> inOrderTraversal(TreeNode root, ArrayList<Integer> array) {
+         if (root == null)
+             return null;
+         inOrderTraversal(root.left, array);
+         array.add(root.val);
+         inOrderTraversal(root.right, array);
+         return array;
+     }
 }
