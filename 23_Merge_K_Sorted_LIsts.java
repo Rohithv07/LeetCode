da01cc2ac4accae631693b2c634fb8f321b0249a
rohithv07 <rohithv63@gmail.com>
2021-01-24 17:11:58 +0530

    Leetcode 23 problem java

---
+++ 23_Merge_K_Sorted_LIsts.java
@@ -0,0 +1,61 @@
+// minHeap
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (ListNode head : lists) {
+            while (head != null) {
+                minHeap.add(head.val);
+                head = head.next;
+            }
+        }
+        ListNode result = new ListNode(0);
+        ListNode current = result;
+        while (!minHeap.isEmpty()) {
+            current.next = new ListNode(minHeap.remove());
+            current = current.next;
+        }
+        return result.next;
+    }
+}
+
+
+
+// divide and conquer
+public static ListNode mergeKLists(ListNode[] lists){
+    return partion(lists,0,lists.length-1);
+}
+
+public static ListNode partion(ListNode[] lists,int s,int e){
+    if(s==e)  return lists[s];
+    if(s<e){
+        int q=(s+e)/2;
+        ListNode l1=partion(lists,s,q);
+        ListNode l2=partion(lists,q+1,e);
+        return merge(l1,l2);
+    }else
+        return null;
+}
+
+//This function is from Merge Two Sorted Lists.
+public static ListNode merge(ListNode l1,ListNode l2){
+    if(l1==null) return l2;
+    if(l2==null) return l1;
+    if(l1.val<l2.val){
+        l1.next=merge(l1.next,l2);
+        return l1;
+    }else{
+        l2.next=merge(l1,l2.next);
+        return l2;
+    }
+}
