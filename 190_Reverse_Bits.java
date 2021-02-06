9b6ef2ce219450c96d8ceee205ee389d2a021c3f
rohithv07 <rohithv63@gmail.com>
2021-02-06 17:15:25 +0530

    Leetcode 190 problem java

---
+++ 190_Reverse_Bits.java
@@ -0,0 +1,13 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int result = 0;
+        for (int i=0; i<32; i++) {
+            result <<= 1;
+            if ((n & 1) == 1)
+                result += 1;
+            n >>= 1;
+        }
+        return result;
+    }
+}
