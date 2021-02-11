48232ed6efcfa521c5a8f7fd29e58eb02f22e71f
rohithv07 <rohithv63@gmail.com>
2021-02-11 22:40:38 +0530

    Leetcode 547 problem java

--- 547_friends_circles.java
+++ 547_friends_circles.java
@@ -29,3 +29,64 @@ class Solution {
         }
     }
 }
+
+
+
+// union find
+
+class Solution {
+    public int findCircleNum(int[][] M) {
+        UnionFind uf = new UnionFind(M.length);
+        for (int i=0; i<M.length - 1; i++) {
+            for (int j=i+1; j<M.length; j++) {
+                if (M[i][j] == 1)
+                    uf.union(i, j);
+            }
+        }
+        return uf.getComponent();
+    }
+}
+
+class UnionFind {
+    int size;
+    int component;
+    int [] parent;
+    int [] rank;
+    public UnionFind(int n) {
+        if (n <= 0)
+            throw new IllegalArgumentException("Error");
+        size = n;
+        component = n;
+        parent = new int [n];
+        rank = new int [n];
+        for (int i=0; i<n; i++)
+            parent[i] = i;
+    }
+    public int find(int p) {
+        while (p != parent[p]) {
+            parent[p] = parent[parent[p]];
+            p = parent[p];
+        }
+        return p;
+    }
+    
+    public void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ)
+            return;
+        if (rank[rootQ] > rank[rootP]) {
+            parent[rootP] = rootQ;
+        }
+        else {
+            parent[rootQ] = rootP;
+            if (rank[rootP] == rank[rootQ])
+                rank[rootP] += 1;
+        }
+        component -= 1;
+    }
+    
+    public int getComponent() {
+        return component;
+    }
+}
