7848783921d8e80b11ad8ced4debd1c687a50a3b
rohithv07 <rohithv63@gmail.com>
2021-02-12 11:29:38 +0530

    Leetcode 64 problem java

--- 64_min_path_sum.java
+++ 64_min_path_sum.java
@@ -14,3 +14,30 @@ class Solution {
         return grid[grid.length-1][grid[0].length-1];
     }
 }
+
+
+
+// another approach
+
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                if (i == 0 && j != 0) {
+                    grid[i][j] = grid[i][j] + grid[i][j - 1];
+                }
+                else if (i != 0 && j == 0) {
+                    grid[i][j] = grid[i - 1][j] + grid[i][j];
+                }
+                else if (i == 0 && j == 0) {
+                    continue;
+                }
+                else
+                    grid[i][j] = Math.min(grid[i-1][j], grid[i][j - 1]) + grid[i][j];
+            }
+        }
+        return grid[row-1][col-1];
+    }
+}
