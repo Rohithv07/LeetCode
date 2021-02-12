baa0b1ca29ab12b131c248092e2c304fe6e871db
rohithv07 <rohithv63@gmail.com>
2021-02-12 11:40:44 +0530

    Leetcode 322 problem java

--- 322_Coin_Change.java
+++ 322_Coin_Change.java
@@ -18,3 +18,30 @@ class Solution {
         return dp[amount] > amount ? -1:dp[amount];
     }
 }
+
+
+
+// Another
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if (amount == 0)
+            return 0;
+        int max = amount + 1;
+        int [] dp = new int [amount + 1];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+        for (int i=1; i<=amount; i++) {
+            for (int j=0; j<coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
+
+
+
+
