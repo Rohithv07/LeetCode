cdbcf1bea1163669795857c8fc9d02be65b124ce
rohithv07 <rohithv63@gmail.com>
2021-02-26 19:58:32 +0530

    Leetcode 234 problem java

---
+++ 234_Palindrome_Linked_List.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null)
+            return true;
+        // find the middle
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow = reverseLinkedList(slow);
+        fast = head;
+        while (slow != null) {
+            if (slow.val != fast.val)
+                return false;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+    
+    public ListNode reverseLinkedList(ListNode slow) {
+        if (slow == null)
+            return null;
+        ListNode current = slow;
+        ListNode previous = null;
+        while (current != null) {
+            ListNode currentNext = current.next;
+            current.next = previous;
+            previous = current;
+            current = currentNext;
+        }
+        return previous;
+    }
+}
