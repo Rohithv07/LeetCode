e4d15eebc20a6cdafca8337cfc7e66caca7a778b
rohithv07 <rohithv63@gmail.com>
2021-03-19 12:05:27 +0530

    Leetcode 35 problem java

---
+++ 35_Search_Insert_Position.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int length = nums.length;
+        int left = 0;
+        int right = length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+                return mid;
+            if (nums[mid] > target)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        return left;
+    }
+}
