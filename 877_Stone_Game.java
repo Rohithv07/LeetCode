221382382f4455d131c55f15302b22f44909100b
rohithv07 <rohithv63@gmail.com>
2021-04-11 22:25:57 +0530

    Leetcode 877 problem java

--- 877_Stone_Game.java
+++ 877_Stone_Game.java
@@ -13,3 +13,31 @@ class Solution {
         return dp[0][n-1] > 0;
     }
 }
+
+
+// using memoisation
+
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        int length = piles.length;
+        int [] sum = new int [length];
+        int [][] cache = new int [length][length];
+        sum[0] = piles[0];
+        for (int i=1; i<length; i++) {
+            sum[i] = sum[i - 1] + piles[i];
+        }
+        int alex = findPoint(piles, sum, cache, 0, length - 1);
+        return alex > sum[length - 1] - alex;
+    }
+    
+    public int findPoint(int [] piles, int [] sum, int [][] cache, int start, int end) {
+        if (start > end)
+            return 0;
+        if (cache[start][end] != 0)
+            return cache[start][end];
+        int currentSum = sum[end] - sum[start] + piles[start];
+        int point = currentSum - Math.min(findPoint(piles, sum, cache, start + 1, end), findPoint(piles, sum, cache, start, end - 1));
+        cache[start][end] = point;
+        return point;
+    }
+}
