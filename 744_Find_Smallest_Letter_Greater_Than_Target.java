78240a27f8bb56fc6909492382591a825c2cb3e8
rohithv07 <rohithv63@gmail.com>
2021-04-17 00:06:52 +0530

    Leetcode 744 problem java

---
+++ 744_Find_Smallest_Letter_Greater_Than_Target.java
@@ -0,0 +1,16 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int low = 0;
+        int high = letters.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (letters[mid] > target) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return low == letters.length ? letters[0] : letters[low];
+    }
+}
