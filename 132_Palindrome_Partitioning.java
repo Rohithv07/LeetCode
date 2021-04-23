f9cb8a9af7cefdb0ac42c6db8ed32b2ea6fe555b
rohithv07 <rohithv63@gmail.com>
2021-04-23 09:55:19 +0530

    Leetcode 132 problem java

--- 132_Palindrome_Partitioning.java
+++ 132_Palindrome_Partitioning.java
@@ -1,3 +1,5 @@
+// this can be further optimised
+
 class Solution {
     private Integer[][] dp;
     private Boolean [][] palindromeString;
@@ -32,3 +34,41 @@ class Solution {
         return true;
     }
 }
+
+// optimised code
+
+class Solution {
+    private Integer[][] dp;
+    private Boolean [][] palindrome;
+    
+    public int minCut(String s) {
+        int length = s.length();
+        dp = new Integer[length][length];
+        palindrome = new Boolean[length][length];
+        return helper(s, 0, length - 1);
+    }
+    
+    public int helper(String s, int start, int end) {
+        if (start ==  end || isPalindrome(s, start, end))
+            return 0;
+        if (dp[start][end] != null)
+            return dp[start][end];
+        int cut = s.length() - 1;
+        for (int i=start; i<=end; i++) {
+            if (isPalindrome(s, start, i)) {
+                cut = Math.min(cut, 1 + helper(s, i + 1, end));
+            }
+        }
+        dp[start][end] = cut;
+        return cut;
+    }
+    
+    public Boolean isPalindrome(String s, int start, int end) {
+        if (start >= end)
+            return true;
+        if (palindrome[start][end] != null) {
+            return palindrome[start][end];
+        }
+        return palindrome[start][end] = (s.charAt(start) == s.charAt(end)) && isPalindrome(s, start+1, end-1);
+    }
+} 
