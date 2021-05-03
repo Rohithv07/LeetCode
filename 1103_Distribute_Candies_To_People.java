c49b1d6d896074fd3b6655c312f179a2d70820f0
rohithv07 <rohithv63@gmail.com>
2021-05-03 23:37:42 +0530

    Leetcode 1103 problem java

--- 1103_Distribute_Candies_To_People.java
+++ 1103_Distribute_Candies_To_People.java
@@ -1,5 +1,29 @@
 class Solution {
     public int[] distributeCandies(int candies, int num_people) {
+        int [] result = new int [num_people];
+        int index = 0;
+        int candyTrack = 1;
+        while (candies > 0) {
+            result[index % num_people] += candyTrack;
+            index += 1;
+            candies -= candyTrack;
+            candyTrack += 1;
+        }
+        if (candies < 0) {
+            result[(index - 1) % num_people] += candies;
+        }
+        return result;
+    }
+}
+
+
+
+
+
+
+
+class Solution {
+    public int[] distributeCandies(int candies, int num_people) {
         int [] array = new int[num_people];
         int i = 0;
         int loop = 0;
