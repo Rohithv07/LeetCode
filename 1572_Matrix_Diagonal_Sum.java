53694175c1b13b0e215de27f532ddfdc1e5896f3
rohithv07 <rohithv63@gmail.com>
2021-05-07 19:56:04 +0530

    Leetcode 1572 problem java

--- 1572_Matrix_Diagonal_Sum.java
+++ 1572_Matrix_Diagonal_Sum.java
@@ -1,6 +1,17 @@
 // A simple point to directly get the sum of primary and secondary sum.
 
-sum += mat[i][i] + mat[i][n - 1 - i];
+// sum += mat[i][i] + mat[i][n - 1 - i];
+
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int n = mat.length;
+        int sum = 0;
+        for (int i=0; i<n; i++) {
+            sum += mat[i][i] + mat[i][n - i - 1];
+        }
+        return n % 2 == 0 ? sum : sum - mat[n / 2][n / 2];
+    }
+}
 
 
 class Solution {
@@ -24,5 +35,25 @@ class Solution {
         int mid = (mat.length-1) / 2;
         int intermediateSum = mat[mid][mid];
         return primarySum + secondarySum - intermediateSum;
+    }
+}
+
+
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        if (mat.length == 1)
+            return mat[0][0];
+        int sum = 0;
+        for (int i=0; i<mat.length; i++) {
+            for (int j=0; j<mat[0].length; j++) {
+                if (i == j)
+                    sum += mat[i][j];
+                else if ((i + j) == mat.length - 1)
+                    sum += mat[i][j];
+            }
+        }
+        if (mat.length % 2 == 0)
+            return sum;
+        return sum;
     }
 }
