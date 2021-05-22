b99c2263935bf5b8dd75daddb5e8b0823d7213b1
rohithv07 <rohithv63@gmail.com>
2021-05-22 23:34:18 +0530

    Leetcode 706 problem java

--- 706_Design_An_HashMap.java
+++ 706_Design_An_HashMap.java
@@ -105,4 +105,81 @@ class ListNode {
  */
 
 
+// another approach
+
+
+
+class ListNode {
+    int key;
+    int value;
+    ListNode next;
+    ListNode() {};
+    ListNode(int key, int  value) {
+        this.value = value;
+        this.key = key;
+    }
+}
+
+class MyHashMap {
+    
+    private static final int MAX_SIZE = (int) 1e6;
+    private ListNode [] bucket;
+    /** Initialize your data structure here. */
+    public MyHashMap() {
+        bucket = new ListNode[MAX_SIZE];
+    }
+    
+    /** value will always be non-negative. */
+    public void put(int key, int value) {
+        int index = getIndex(key);
+        ListNode previous = getPrevious(index, key);
+        if (previous.next == null) {
+            previous.next = new ListNode(key, value);
+        }
+        else {
+            previous.next.value = value;
+        }
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    public int get(int key) {
+        int index = getIndex(key); // hashcode(key) % bucketlength ==== key % bucketlength
+        ListNode previous = getPrevious(index, key);
+        if (previous.next == null)
+            return -1;
+        return previous.next.value;
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    public void remove(int key) {
+        int index = getIndex(key);
+        ListNode previous = getPrevious(index, key);
+        if (previous.next == null)
+            return;
+        previous.next = null;
+    }
+    
+    public int getIndex(int value) {
+        return value % bucket.length;
+    }
+    
+    public ListNode getPrevious(int index, int key) {
+        if (bucket[index] == null) {
+            return bucket[index] = new ListNode(-1, -1);
+        }
+        ListNode previous = bucket[index];
+        while (previous.next != null && previous.next.key != key)
+            previous = previous.next;
+        return previous;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
+
 
