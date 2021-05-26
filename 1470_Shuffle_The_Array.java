494593c5a85db0b0135821bd7124c123bd71a84d
rohithv07 <rohithv63@gmail.com>
2021-05-26 17:48:27 +0530

    Leetcode 1470 problem java

--- 1470_Shuffle_The_Array.java
+++ 1470_Shuffle_The_Array.java
@@ -11,3 +11,17 @@ class Solution {
         return newArray;
     }
 }
+
+
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int [] result = new int [nums.length];
+        int index = 0;
+        for (int i=0; i<n; i++) {
+            result[index] = nums[i];
+            result[index+1] = nums[nums.length - n + i];
+            index += 2;
+        }
+        return result;
+    }
+}
