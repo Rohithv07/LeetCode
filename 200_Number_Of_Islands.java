aede46a41fce7b43e9c709c28b1756b982b0c501
rohithv07 <rohithv63@gmail.com>
2021-05-29 17:42:37 +0530

    Leetcode 200 problem java

--- 200_Number_Of_Islands.java
+++ 200_Number_Of_Islands.java
@@ -1,3 +1,5 @@
+
+// dfs
 class Solution {
     public int numIslands(char[][] grid) {
         if (grid.length == 0 || grid == null)
@@ -21,5 +23,48 @@ class Solution {
         dfsHelper(grid, i, j-1);
         return 1;
         
+    }
+}
+
+// bfs
+
+class Solution {
+    
+    private int [][] directions = new int [][] {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    
+    public int numIslands(char[][] grid) {
+        if (grid.length == 0 || grid == null)
+            return 0;
+        int row = grid.length;
+        int col = grid[0].length;
+        int islands = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                if (grid[i][j] == '1') {
+                    grid[i][j] = '0';
+                    queue.offer(new int [] {i, j});
+                    bfsHelper(grid, queue);
+                    islands += 1;
+                }
+            }
+        }
+        return islands;
+    }
+    
+    public void bfsHelper(char [][] grid, Queue<int[]> queue) {
+        while (!queue.isEmpty()) {
+            int [] current = queue.poll();
+            int x = current[0];
+            int y = current[1];
+            for (int [] dir : directions) {
+                int newX = dir[0] + x;
+                int newY = dir[1] + y;
+                if (newX < 0 || newY < 0 || newX >= grid.length || newY >= grid[0].length || grid[newX][newY] == '0')
+                    continue;
+                grid[newX][newY] = '0';
+                queue.offer(new int []{newX, newY});
+            }
+        }
     }
 }
