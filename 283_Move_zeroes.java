98fa343c2c5160e98a85129e9656ed7886d3c33d
rohithv07 <rohithv63@gmail.com>
2021-06-05 00:11:19 +0530

    Leetcode 283 problem java

--- 283_Move_zeroes.java
+++ 283_Move_zeroes.java
@@ -10,3 +10,25 @@ class Solution {
         }
     }
 }
+
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left = 0;
+        int right = 0;
+        while (right < nums.length) {
+            if (nums[right] != 0) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                right++;
+                left++;
+            }
+            else {
+                right++;
+            }
+        }
+    }
+}
+
+// in - place and minimum number of operations                 
