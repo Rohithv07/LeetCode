8726958e94ae567b07a99ca08b1931dd55556a31
rohithv07 <rohithv63@gmail.com>
2021-06-06 10:02:47 +0530

    Leetcode 215 problem java

--- 215_Kth_Largest_Element_In_An_array.java
+++ 215_Kth_Largest_Element_In_An_array.java
@@ -12,3 +12,46 @@ class Solution {
 }
 
 // nlogk
+
+
+
+// quickselect
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        if (nums == null || nums.length == 0)
+            return Integer.MAX_VALUE;
+        // considering last element as the pivot
+        return quickSelect(nums, 0, nums.length - 1, nums.length - k);
+    }
+    
+    public int quickSelect(int [] nums, int start, int end, int k) {
+        if (start > end)
+            return Integer.MAX_VALUE;
+        int pivot = nums[end];
+        int left = start;
+        for (int i=start; i<end; i++) {
+            if (pivot >= nums[i]) {
+                swap(nums, left++, i);
+            }
+        }
+        swap(nums, left, end);
+        if (left == k)
+            return nums[left];
+        if (left < k)
+            return quickSelect(nums, left+1, end, k);
+        return quickSelect(nums, start, left-1, k);
+    }
+    
+    public void swap(int [] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
+
+
+ indexOfPivot == k, return A[k]
+else if indexOfPivot < k, keep checking right part to pivot
+else if indexOfPivot > k, keep checking left part to pivot
+
