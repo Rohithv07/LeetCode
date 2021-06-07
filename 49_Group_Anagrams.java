26241f4f7be65af14c56f3e6d5638e507a1e7295
rohithv07 <rohithv63@gmail.com>
2021-06-07 23:40:00 +0530

    Leetcode 49 problem java

--- 49_Group_Anagrams.java
+++ 49_Group_Anagrams.java
@@ -21,3 +21,38 @@ class Solution {
         return new ArrayList(result.values());
     }
 }
+
+// some comments
+
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        if (strs == null || strs.length == 0)
+            return result;
+        // a map to know about the count of chars and matching list of words
+        Map<String, List> map = new HashMap<>();
+        int [] count = new int [26];
+        for (String s : strs) {
+            // initialising count array to 0 for every word
+            Arrays.fill(count, 0);
+            for (char ch : s.toCharArray()) {
+                count[ch - 'a'] += 1;
+            }
+            StringBuilder sb = new StringBuilder();
+            // creating a unique key appending * and count as the count will be same for anagrams
+            for (int i=0; i<26; i++) {
+                sb.append("*");
+                sb.append(count[i]);
+            }
+            String key = sb.toString();
+            // if the key not present, we put it into the map and create an emoty list
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            // we getr the key and add the string
+            map.get(key).add(s);
+        }
+        return new ArrayList(map.values());
+    }
+}
