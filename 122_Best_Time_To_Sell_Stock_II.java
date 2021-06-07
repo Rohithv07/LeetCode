e8c92bc7dfc045251921ba86b131fb71766c96a8
rohithv07 <rohithv63@gmail.com>
2021-06-07 23:25:18 +0530

    Leetcode 122 problem java

--- 122_Best_Time_To_Sell_Stock_II.java
+++ 122_Best_Time_To_Sell_Stock_II.java
@@ -8,3 +8,20 @@ class Solution {
         return max_without_stock;
     }
 }
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        // valley peak approach
+        // if the current element is greater than previous element, we add the profit assuming we sell on current element and buy on previous element
+        // else we just skip it
+        if (prices == null || prices.length <= 1)
+            return 0;
+        int profit = 0;
+        for (int i=1; i<prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += (prices[i] - prices[i - 1]);
+            }
+        }
+        return profit;
+    }
+}
