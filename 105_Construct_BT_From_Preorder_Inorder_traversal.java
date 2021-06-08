6296f573487032e85a16ce4f72bead1dd5b11ba9
rohithv07 <rohithv63@gmail.com>
2021-06-08 14:48:44 +0530

    Leetcode 105 problem java

--- 105_Construct_BT_From_Preorder_Inorder_traversal.java
+++ 105_Construct_BT_From_Preorder_Inorder_traversal.java
@@ -40,3 +40,43 @@ class Solution {
         return tree;
     }
 }
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int pre = 0;
+    private int in = 0;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return construct(preorder, inorder, Integer.MIN_VALUE);
+    }
+    
+    public TreeNode construct(int [] preorder, int [] inorder, int stop) {
+        if (pre >= preorder.length)
+            return null;
+        if (inorder[in] == stop) {
+            in++;
+            return null;
+        }
+        TreeNode node = new TreeNode(preorder[pre++]);
+        node.left = construct(preorder, inorder, node.val);
+        node.right = construct(preorder, inorder, stop);
+        
+        return node;
+    }
+}
