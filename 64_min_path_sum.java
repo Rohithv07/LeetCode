18046950f42a407f43c5495a2555df907ddd2297
rohithv07 <rohithv63@gmail.com>
2021-06-23 16:29:51 +0530

    Leetcode 64 problem java

--- 64_min_path_sum.java
+++ 64_min_path_sum.java
@@ -41,3 +41,35 @@ class Solution {
         return grid[row-1][col-1];
     }
 }
+
+// recursion + memoizartion
+
+class Solution {
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0)
+            return 0;
+        int row = grid.length;
+        int col = grid[0].length;
+        int [][] dp = new int [row][col];
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        return memo(grid, row - 1, col - 1, dp);
+    }
+    
+    public int memo(int [][] grid, int row, int col, int [][] dp) {
+        if (dp[row][col] != -1) {
+            return dp[row][col];
+        }
+        if (row == 0 && col == 0)
+            return grid[row][col];
+        if (row == 0)
+            return grid[row][col] + memo(grid, row, col-1, dp);
+        if (col == 0)
+            return grid[row][col] + memo(grid, row - 1, col, dp);
+        dp[row][col] = grid[row][col] + Math.min(memo(grid, row, col-1, dp), memo(grid, row-1, col, dp));
+        return dp[row][col];
+    }
+}
