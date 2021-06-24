122e8bcff0b01e2e693f902ef19c26557d805931
rohithv07 <rohithv63@gmail.com>
2021-06-24 23:30:58 +0530

    Leetcode 1004 problem java

--- 1004_Max_Consecutive_Ones.java
+++ 1004_Max_Consecutive_Ones.java
@@ -1,4 +1,25 @@
 class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int low = 0;
+        int maxLength = 0;
+        int length = nums.length;
+        int countZero = 0;
+        for (int i=0; i<length; i++) {
+            if (nums[i] == 0) {
+                countZero += 1;
+            }
+            while (countZero > k) {
+                if (nums[low] == 0)
+                    countZero -= 1;
+                low += 1;
+            }
+            maxLength = Math.max(maxLength, i - low + 1);
+        }
+        return maxLength;
+    }
+}
+
+class Solution {
     public int longestOnes(int[] A, int k) {
         int start = 0;
         int end;
