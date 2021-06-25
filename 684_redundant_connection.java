f5a50add638fd37c021f1566b00d0e973b68e838
rohithv07 <rohithv63@gmail.com>
2021-06-25 16:21:11 +0530

    Leetcode 684 problem java

--- 684_redundant_connection.java
+++ 684_redundant_connection.java
@@ -1,5 +1,73 @@
 class Solution {
     public int[] findRedundantConnection(int[][] edges) {
+        int length = edges.length;
+        UnionFind uf = new UnionFind(length);
+        int component = uf.getComponent();
+        for (int [] edge: edges) {
+            int p1 = uf.find(edge[0] - 1);
+            int p2 = uf.find(edge[1] - 1);
+            if (p1 == p2) {
+                return new int [] {edge[0], edge[1]};
+            }
+            else {
+                uf.union(edge[0] - 1, edge[1] - 1);
+            }
+        }
+        return new int [] {};
+    }
+}
+
+class UnionFind {
+    int component;
+    int size;
+    int [] parent;
+    int [] rank;
+    UnionFind(int n) {
+        if (n <= 0)
+            throw new IllegalArgumentException("n can't be negative");
+        size = n;
+        component = n;
+        parent = new int [n];
+        rank = new int [n];
+        for (int i=0; i<n; i++) {
+            parent[i] = i;
+        }
+    }
+    
+    int find(int p) {
+        while (p != parent[p]) {
+            parent[p] = parent[parent[p]];
+            p = parent[p];
+        }
+        return p;
+    }
+    
+    void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ)
+            return;
+        if (rank[rootP] < rank[rootQ]) {
+            parent[rootP] = rootQ;
+        }
+        else {
+            parent[rootQ] = rootP;
+            if (rank[rootP] == rank[rootQ]) {
+                rank[rootP] += 1;
+            }
+        }
+        component -= 1;
+    }
+    
+    int getComponent() {
+        return component;
+    }
+}
+
+
+
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
         if (edges.length == 0)
             return null;
         int [] parent = new int[edges.length+1];
