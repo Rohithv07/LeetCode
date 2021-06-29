56884a102c04dd8775187bb3a5683b208f501a78
rohithv07 <rohithv63@gmail.com>
2021-06-30 00:30:33 +0530

    Leetcode 221 problem java

--- 221_Maximal_Square.java
+++ 221_Maximal_Square.java
@@ -51,3 +51,25 @@ class Solution {
         return max * max;
     }
 }
+
+
+
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length == 0)
+            return 0;
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int [][] dp = new int [row + 1][col + 1];
+        int max = 0;
+        for (int i=1; i<=row; i++) {
+            for (int j=1; j<=col; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1]));
+                }
+                max = Math.max(dp[i][j], max);
+            }
+        }
+        return max * max;
+    }
+}
