db99ac5fa5fa3f955bb3d5a6068eeb6216b99bf3
rohithv07 <rohithv63@gmail.com>
2021-07-01 16:27:13 +0530

    Leetcode 524 problem java

--- 524_Longest_Word_In_Dictionary_Through_Deleting.java
+++ 524_Longest_Word_In_Dictionary_Through_Deleting.java
@@ -60,19 +60,45 @@ class Solution {
 // inorder to optimise, we can remove sorting and use compareTo in java
 
 
+// optimised witout sorting
 class Solution {
-    public String findLongestWord(String s, List<String> d) {
-    String longest = "";
-    for (String dictWord : d) {
-        int i = 0;
-        for (char c : s.toCharArray()) 
-            if (i < dictWord.length() && c == dictWord.charAt(i)) i++;
-
-        if (i == dictWord.length() && dictWord.length() >= longest.length()) 
-            if (dictWord.length() > longest.length() || dictWord.compareTo(longest) < 0)
-                longest = dictWord;
+    public String findLongestWord(String s, List<String> dictionary) {
+        if (s == null || s.length() == 0)
+            return "";
+        int index = -1;
+        int max = 0;
+        String longest = "";
+        for (int i=0; i<dictionary.size(); i++) { // O(sizeOfDictiaonry)
+            String current = dictionary.get(i); // O(n + max(wordslkengthindict))
+            if (isSubsequence(s, current)) {
+                int length = current.length();
+                if (length >= max) {
+                    if (length > max || current.compareTo(longest) < 0) {
+                        max = length;
+                        index = i;
+                        longest = current;
+                    }
+                }
+            }
+        }
+        return index == -1 ? "" : dictionary.get(index);
     }
-    return longest;
-}
+    
+    public boolean isSubsequence(String s1, String s2) {
+        int length1 = s1.length();
+        int length2 = s2.length();
+        int i = 0;
+        int j = 0;
+        while (i < length1 && j < length2) {
+            if (s1.charAt(i) == s2.charAt(j)) {
+                i++;
+                j++;
+            }
+            else {
+                i++;
+            }
+        }
+        return j == length2 ? true : false;
+    }
 }
 
