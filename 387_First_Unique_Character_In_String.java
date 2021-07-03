6052ef3755be2b7704a73c650e6b8245face9005
rohithv07 <rohithv63@gmail.com>
2021-07-03 11:04:28 +0530

    Leetcode 387 problem java

--- 387_First_Unique_Character_In_String.java
+++ 387_First_Unique_Character_In_String.java
@@ -13,3 +13,19 @@ class Solution {
         return -1;
     }
 }
+
+class Solution {
+    public int firstUniqChar(String s) {
+        if (s == null || s.length() == 0)
+            return -1;
+        int [] count = new int [26];
+        for (char ch : s.toCharArray()) {
+            count[ch - 'a'] += 1;
+        }
+        for (int i=0; i<s.length(); i++) {
+            if (count[s.charAt(i) - 'a'] == 1)
+                return i;
+        }
+        return -1;
+    }
+}
