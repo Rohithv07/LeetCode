192e9d8ab36fdfd6600494d3db5e10039d8fc86f
rohithv07 <rohithv63@gmail.com>
2021-07-04 00:16:09 +0530

    Leetcode 993 problem java

--- 993_Cousins_In_Binary_Tree.java
+++ 993_Cousins_In_Binary_Tree.java
@@ -49,3 +49,52 @@ class Solution {
         return false;
     }
 }
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int leftDepth = -1;
+    private int rightDepth = -2;
+    // because the depth should not be equal
+    private TreeNode leftParent = null;
+    private TreeNode rightParent = null;
+    public boolean isCousins(TreeNode root, int x, int y) {
+        if (root == null)
+            return false;
+        dfsHelper(root, null, x, y, 0);
+        return leftDepth == rightDepth && leftParent != rightParent;
+    }
+    
+    public void dfsHelper(TreeNode node, TreeNode parent, int x, int y, int depth) {
+        if (node == null)
+            return;
+        if (node.val == x) {
+            leftParent = parent;
+            leftDepth = depth;
+        }
+        else if (node.val == y) {
+            rightParent = parent;
+            rightDepth = depth;
+        }
+        else {
+            // check for left and right subtree
+            dfsHelper(node.left, node, x, y, depth + 1);
+            dfsHelper(node.right, node, x, y, depth + 1);
+        }
+    }
+}
