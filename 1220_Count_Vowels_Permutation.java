ec997ceb8f0095cd6a1761e1c3999263720aeef1
rohithv07 <rohithv63@gmail.com>
2021-07-04 19:41:15 +0530

    Leetcode 1220 problem java

--- 1220_Count_Vowels_Permutation.java
+++ 1220_Count_Vowels_Permutation.java
@@ -35,3 +35,39 @@ class Solution {
         return (int) answer;
     }
 }
+
+
+// 0 based index so we dont want take n + 1
+
+class Solution {
+    public int countVowelPermutation(int n) {
+        if (n == 1)
+            return 5;
+        if (n == 2)
+            return 10;
+        int mod = (int)1e9 + 7;
+        long [][] dp = new long [n][5];
+        for (int i=0; i<5; i++) {
+            dp[0][i] = 1;
+        }
+        /*
+        a - 0 -> e i u
+        e - 1 -> a i
+        i - 2 -> e o
+        o - 3 -> i
+        u - 4 -> o i
+        */
+        for (int i=1; i<n; i++) {
+            dp[i][0] = (dp[i - 1][1] + dp[i - 1][2] + dp[i - 1][4]) % mod;
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % mod;
+            dp[i][2] = (dp[i - 1][1] + dp[i - 1][3]) % mod;
+            dp[i][3] = (dp[i - 1][2]) % mod;
+            dp[i][4] = (dp[i - 1][3] + dp[i - 1][2]) % mod;
+        }
+        long answer = 0;
+        for (int i=0; i<5; i++) {
+            answer = (answer + dp[n - 1][i]) % mod;
+        }
+        return (int)answer;
+    }
+}
