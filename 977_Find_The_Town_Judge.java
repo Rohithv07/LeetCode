ab49e19938d808ccaa941425b6cadf73247a108a
rohithv07 <rohithv63@gmail.com>
2021-07-06 00:14:57 +0530

    Leetcode 977 problem java

--- 977_Find_The_Town_Judge.java
+++ 977_Find_The_Town_Judge.java
@@ -1,3 +1,21 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int [] trusting = new int [n + 1];
+        int [] trustedBy = new int [n + 1];
+        for (int [] t : trust) {
+            trusting[t[0]] += 1;
+            trustedBy[t[1]] += 1;
+        }
+        // for judge trustedBY == n-1 and trusting = 0 as judge didnt trust anybody
+        for (int i=1; i<=n; i++) {
+            if (trustedBy[i] == n - 1 && trusting[i] == 0)
+                return i;
+        }
+        return -1;
+    }
+}
+
+
 // in-degree - out-degree = N - 1 become the judge.
 
 class Solution {
