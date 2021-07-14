86afa260f4e29fe1be7b7b50c0896ced24bd4ae1
Rohithv07 <rohithv63@gmail.com>
2021-07-14 15:54:41 +0530

    Leetcode 1122 problem java

--- 1122_Relative_Sort_Array.java
+++ 1122_Relative_Sort_Array.java
@@ -23,3 +23,33 @@ class Solution {
         return arr1;
     }
 }
+
+// more efficient using the constraints given
+
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        if (arr2 == null || arr2.length == 0)
+            return arr1;
+        if (arr1 == null || arr1.length == 0)
+            return new int []{};
+        int [] result = new int [arr1.length];
+        int [] count = new int [1001];
+        for (int number : arr1) {
+            count[number] += 1;
+        }
+        int index = 0;
+        for (int number : arr2) {
+            while (count[number]-- > 0) {
+                result[index++] = number;
+            }
+        }
+        for (int i=0; i<=1000; i++) {
+            while (count[i]-- > 0) {
+                result[index++] = i;
+            }
+        }
+        return result;
+    }
+}
+
+// similar to custom string sorting problem 791
