cc057d8aaa5f7dad24ab1448229576da17dc3ac3
Rohithv07 <rohithv63@gmail.com>
2021-07-16 23:35:49 +0530

    Leetcode 451 problem java

--- 451_Sort_Characters_By_Frequency.java
+++ 451_Sort_Characters_By_Frequency.java
@@ -23,3 +23,49 @@ class Solution {
         return sb.toString();
     }
 }
+
+// without using hashmap but a count array
+
+class Solution {
+    public String frequencySort(String s) {
+        if (s == null || s.length() == 0)
+            return "";
+        int length = s.length();
+        Pair [] count = new Pair[256];
+        for (char ch : s.toCharArray()) {
+            if (count[ch] == null)
+                count[ch] = new Pair(ch, 1);
+            else
+                count[ch].freq += 1;
+        }
+        PriorityQueue<Pair> pq = new PriorityQueue<>((p1, p2) -> Integer.compare(p2.freq, p1.freq));
+        for (Pair p : count) {
+            if (p != null) {
+                pq.add(p);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!pq.isEmpty()) {
+            Pair currentPair = pq.poll();
+            int currentFreq = currentPair.freq;
+            for (int i=0; i<currentFreq; i++) {
+                sb.append(currentPair.ch);
+            }
+        }
+        // for (Pair p : count) {
+        //     if (p != null)
+        //     System.out.println(p.ch + " " + p.freq);
+        // }
+        return sb.toString();
+    }
+}
+
+class Pair {
+    char ch;
+    int freq;
+    
+    public Pair(char ch, int freq) {
+        this.ch = ch;
+        this.freq = freq;
+    }
+}
