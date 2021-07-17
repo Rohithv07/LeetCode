b0a90a1de1df813ce6b8825903f1173860eab363
Rohithv07 <rohithv63@gmail.com>
2021-07-17 23:52:40 +0530

    Leetcode 1008 problem java

--- 1008_Construct_BST_From_Preorder_Traversal.java
+++ 1008_Construct_BST_From_Preorder_Traversal.java
@@ -84,5 +84,50 @@ class Solution {
          return root;
      }
  }
+
+ // using stack iterative
+
+ /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode bstFromPreorder(int[] preorder) {
+        if (preorder == null || preorder.length == 0)
+            return new TreeNode();
+        int rootElement = preorder[0];
+        int length = preorder.length;
+        TreeNode root = new TreeNode(rootElement);
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        for (int i=1; i<length; i++) {
+            int currentNode = preorder[i];
+            TreeNode node = new TreeNode(currentNode);
+            if (stack.peek().val > currentNode) {
+                stack.peek().left = node;
+            }
+            else {
+                TreeNode parent = stack.peek();
+                while (!stack.isEmpty() && currentNode > stack.peek().val) {
+                    parent = stack.pop();
+                }
+                parent.right = node;
+            }
+            stack.push(node);
+        }
+        return root;
+    }
+}
     
     
