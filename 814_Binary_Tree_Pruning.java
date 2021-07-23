e458c542a56677d06340c04cd9e134ede62660ca
Rohithv07 <rohithv63@gmail.com>
2021-07-23 15:23:40 +0530

    Leetcode 814 problem java

---
+++ 814_Binary_Tree_Pruning.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null)
+            return null;
+        TreeNode left = pruneTree(root.left);
+        root.left = left;
+        TreeNode right = pruneTree(root.right);
+        root.right = right;
+        if (root.val == 0 && root.left == null && root.right == null)
+            return null;
+        return root;
+    }
+}
