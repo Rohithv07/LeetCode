c403a14785bc9acc07bcbc6dae381359ba2a54e6
Rohithv07 <rohithv63@gmail.com>
2021-07-27 00:17:40 +0530

    Leetcode 231 problem java

--- 231_Power_Of_Two.java
+++ 231_Power_Of_Two.java
@@ -16,3 +16,22 @@ class Solution {
         return (n & (n - 1)) == 0;
     }
 }
+
+
+class Solution {
+    public boolean isPowerOfTwo(int num) {
+        if (num <= 0)
+            return false;
+        if (num == 1)
+            return true;
+        int x = 1;
+        int count = 0;
+        while (num > 0) {
+            int value = num & x;
+            if (value == 1)
+                count += 1;
+            num = num >> 1;
+        }
+        return count == 1;
+    }
+}
