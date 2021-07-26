9bbc6488c489f2688e16921f5a4b301dcc11248e
Rohithv07 <rohithv63@gmail.com>
2021-07-27 00:08:41 +0530

    Leetcode 231 problem java

--- 231_Power_Of_Two.java
+++ 231_Power_Of_Two.java
@@ -5,3 +5,14 @@ class Solution {
         return (Math.log10(n) / Math.log10(2)) % 1 == 0;
     }
 }
+
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0)
+            return false;
+        if (n == 1)
+            return true;
+        return (n & (n - 1)) == 0;
+    }
+}
