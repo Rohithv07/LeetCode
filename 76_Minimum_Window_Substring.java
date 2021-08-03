b9677cfffaf16cc039aae67d3f96ac214864542d
Rohithv07 <rohithv63@gmail.com>
2021-08-03 14:56:19 +0530

    Leetcode 76 problem java

--- 76_Minimum_Window_Substring.java
+++ 76_Minimum_Window_Substring.java
@@ -36,3 +36,51 @@ class Solution {
         return length == Integer.MAX_VALUE ? "" : s.substring(head, head + length);
     }
 }
+
+
+// constant space using array
+
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s.length() < t.length())
+            return "";
+        int [][] count = new int [256][2];
+        for (char ch : t.toCharArray()) {
+            count[ch][0] += 1;
+            count[ch][1] = 1;
+        }
+        int counter = 0;
+        for (int [] number : count) {
+            if (number[1] == 1)
+                counter += 1;
+        }
+        int left = 0;
+        int right = 0;
+        int length = Integer.MAX_VALUE;
+        int head = 0;
+        while (right < s.length()) {
+            char currentRight = s.charAt(right);
+            if (count[currentRight][1] == 1) {
+                count[currentRight][0] -= 1;
+                if (count[currentRight][0] == 0)
+                    counter -= 1;
+            }
+            right ++;
+            while (counter == 0) {
+                char currentLeft = s.charAt(left);
+                if (count[currentLeft][1] == 1) {
+                    count[currentLeft][0] += 1;
+                    if (count[currentLeft][0] > 0) {
+                        counter += 1;
+                    }
+                }
+                if (right - left < length) {
+                    length = right - left;
+                    head = left;
+                }
+                left++;
+            }
+        }
+        return length == Integer.MAX_VALUE ? "" : s.substring(head, head + length);
+    }
+}
