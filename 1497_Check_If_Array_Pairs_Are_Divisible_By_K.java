5cdcd23d64ddb26c390b444bdb702153c4d912a3
Rohithv07 <rohithv63@gmail.com>
2021-08-08 19:54:45 +0530

    Leetcode 1497 problem java

--- 1497_Check_If_Array_Pairs_Are_Divisible_By_K.java
+++ 1497_Check_If_Array_Pairs_Are_Divisible_By_K.java
@@ -17,3 +17,28 @@ class Solution {
         return true;
     }
 }
+
+
+// same approach different eay
+
+class Solution {
+    public boolean canArrange(int[] arr, int k) {
+        if (arr == null || arr.length == 0 || arr.length % 2 == 1)
+            return false;
+        int length = arr.length;
+        for (int i=0; i<length; i++) {
+            arr[i] = (arr[i] % k + k) % k;
+        }
+        int [] hash = new int [k];
+        for (int i=0; i<length; i++) {
+            hash[arr[i]] += 1;
+        }
+        if (hash[0] % 2 == 1)
+            return false;
+        for (int i=1; i<k; i++) {
+            if (hash[i] != hash[k - i])
+                return false;
+        }
+        return true;
+    }
+}
