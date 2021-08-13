dde1062e3e99b8fb7dfa8320b6afb4239c3880ae
Rohithv07 <rohithv63@gmail.com>
2021-08-13 17:59:01 +0530

    Leetcode 518 problem java

--- 518_Coin_Change_2.java
+++ 518_Coin_Change_2.java
@@ -56,3 +56,24 @@ class Solution {
         
     }
 }
+
+
+// same idea
+
+class Solution {
+    public int change(int amount, int[] coins) {
+        if (coins == null || coins.length == 0 || amount < 0)
+            return 0;
+        int [] dp = new int [amount + 1];
+        dp[0] = 1;
+        for (int i=0; i<coins.length; i++) {
+            for (int j=1; j<=amount; j++) {
+                //dp[j] = dp[j - 1];
+                if (j >= coins[i]) {
+                    dp[j] += dp[j - coins[i]];
+                }
+            }
+        }
+        return dp[amount];
+    }
+}
