1faf8d848822bf27dc0fb9a7045c00b424798eaa
Rohithv07 <rohithv63@gmail.com>
2021-08-19 17:55:30 +0530

    Leetcode 1339 problem java

--- 1339_Max_Prod_Of_Splitted_BT.java
+++ 1339_Max_Prod_Of_Splitted_BT.java
@@ -40,3 +40,42 @@ class Solution {
         return (int)(maxProd % ((int)Math.pow(10, 9) + 7));
     }
 }
+
+
+// slightly diff code
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private long result = 0;
+    private long total = 0;
+    public int maxProduct(TreeNode root) {
+        if (root == null)
+            return 0;
+        final long mod = (long)1e9 + 7;
+        total = dfs(root); // get the total sum
+        dfs(root); // sum of each subtree and its complement
+        return (int)(result % mod);
+    }
+    
+    public int dfs(TreeNode node) {
+        if (node == null)
+            return 0;
+        int currentSum = dfs(node.left) + dfs(node.right) + node.val;
+        result = Math.max(result, (total - currentSum) * currentSum);
+        return currentSum;
+    }
+}
