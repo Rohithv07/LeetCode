10f891040305987731a952e66b3da4aeffc2ef17
Rohithv07 <rohithv63@gmail.com>
2021-08-21 00:15:41 +0530

    Leetcode 1706 problem java

--- 1706_Where_Will_The_BAll_Fall.java
+++ 1706_Where_Will_The_BAll_Fall.java
@@ -23,3 +23,43 @@ class Solution {
         return answer;
     }
 }
+
+
+class Solution {
+    public int[] findBall(int[][] grid) {
+        if (grid == null || grid.length == 0)
+            return new int []{};
+        int row = grid.length;
+        int col = grid[0].length;
+        int [] fallingPos = new int [col];
+        // we start putting the ball on every col of top rwo
+        for (int i=0; i<col; i++) {
+            // we wiill be starting from the top row
+            int currentRow = 0;
+            int currentCol = i;
+            // check whether the ball reacnes down the row
+            while (currentRow < row) {
+                // if current val and value to the right both 1
+                if (grid[currentRow][currentCol] == 1 && currentCol + 1 < col && grid[currentRow][currentCol + 1] == 1) {
+                    currentRow ++;
+                    currentCol ++;
+                }
+                // if the current value and value to left == -1
+                else if (grid[currentRow][currentCol] == -1 && currentCol - 1 >= 0 && grid[currentRow][currentCol - 1] == -1) {
+                    currentRow ++;
+                    currentCol--;
+                }
+                else {
+                    break;
+                }
+            }
+            if (currentRow == row) {
+                fallingPos[i] = currentCol;
+            }
+            else {
+                fallingPos[i] = -1;
+            }
+        }
+        return fallingPos;
+    }
+}
