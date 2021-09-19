798aab33c69a53970a07ca272d456471fd70b669
Rohithv07 <rohithv63@gmail.com>
2021-09-19 17:15:38 +0530

    Leetcode 115 problem java

--- Set1_1000/115_Distinct_Subsequences.java
+++ Set1_1000/115_Distinct_Subsequences.java
@@ -21,3 +21,29 @@ class Solution {
         return dp[tLength][sLength];
     }
 }
+
+
+class Solution {
+    public int numDistinct(String s, String t) {
+        int length1 = s.length();
+        int length2 = t.length();
+        int [][] dp = new int [length1 + 1][length2 + 1];
+        // please consider where we are taking s and t ie in row or col
+        // if s is taken as col then the first row must be filled with 1. Then if two char become equal, dp[i][j] = dp[i - 1][j - 1] + dp[ i -1][j];
+        // if s is taken row then the first col must be filled with 1. Then if two char become equal, dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1];
+        for (int i=0; i<=length1; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i=1; i<=length1; i++) {
+            for (int j=1; j<=length2; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                }
+                else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[length1][length2];
+    }
+}
