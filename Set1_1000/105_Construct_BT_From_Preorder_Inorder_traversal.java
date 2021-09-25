435c822514d7a0ec5061f4053c225da20db54197
Rohithv07 <rohithv63@gmail.com>
2021-09-25 10:42:49 +0530

    Leetcode 105 problem java

--- Set1_1000/105_Construct_BT_From_Preorder_Inorder_traversal.java
+++ Set1_1000/105_Construct_BT_From_Preorder_Inorder_traversal.java
@@ -80,3 +80,45 @@ class Solution {
         return node;
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || preorder.length == 0 || inorder == null || inorder.length == 0) {
+            return null;
+        }
+        return constructTree(0, 0, inorder.length - 1, preorder, inorder);
+    }
+    
+    public TreeNode constructTree(int preIndex, int inStart, int inEnd, int [] preorder, int [] inorder) {
+        if (preIndex >= preorder.length || inStart > inEnd) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[preIndex]);
+        int rootInInorder = -1;
+        for (int i=inStart; i<=inEnd; i++) {
+            if (preorder[preIndex] == inorder[i]) {
+                rootInInorder = i;
+                break;
+            }
+        }
+        root.left = constructTree(preIndex+1, inStart, rootInInorder - 1, preorder, inorder);
+        root.right = constructTree(preIndex + rootInInorder - inStart + 1, rootInInorder + 1, inEnd, preorder, inorder);
+        return root;
+    }
+}
