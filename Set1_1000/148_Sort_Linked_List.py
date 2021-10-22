ae06dc24ae0924c9c5460f0eb6637e8bcd7d3fc7
Rohithv07 <rohithv63@gmail.com>
2021-10-22 18:08:53 +0530

    Leetcode 148 problem python, java

--- Set1_1000/148_Sort_Linked_List.py
+++ Set1_1000/148_Sort_Linked_List.py
@@ -40,3 +40,84 @@ class Solution:
             result.next = self.finalMergeSort(a, b.next)
             
         return result
+
+
+# adding java code here : 
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        #// 1. find the middle
+        #// 2. split the list to left and right wrt to head and the middle
+        #// 3. merge this splitted list
+        #// 4. return the final merged list
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode previous = null;
+        while (fast != null && fast.next != null) {
+            previous = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        previous.next = null;
+        #// ListNode middle = findMiddle(head);
+        #// ListNode nextOfMiddle = middle.next;
+        #// middle.next = null;
+        
+        ListNode left = sortList(head);
+        ListNode right = sortList(slow);
+        
+        ListNode finalMergedList = merge(left, right);
+        return finalMergedList;
+    }
+    
+    """
+    private ListNode findMiddle(ListNode node) {
+        if (node == null || node.next == null) {
+            return node;
+        }
+        ListNode slow = node;
+        ListNode fast = node;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    } 
+    """
+    
+    private ListNode merge(ListNode node1, ListNode node2) {
+        if (node1 == null && node2 == null) {
+            return null;
+        }
+        if (node1 == null) {
+            return node2;
+        }
+        if (node2 == null) {
+            return node1;
+        }
+        ListNode result = null;
+        if (node1.val <= node2.val) {
+            result = node1;
+            result.next = merge(node1.next, node2);
+        }
+        else {
+            result = node2;
+            result.next = merge(node1, node2.next);
+        }
+        return result;
+    }
+}
