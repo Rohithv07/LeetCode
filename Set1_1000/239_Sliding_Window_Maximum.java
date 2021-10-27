38a89b1d65599ca40beebba7c115b3f410751fab
Rohithv07 <rohithv63@gmail.com>
2021-10-27 23:45:47 +0530

    Leetcode 239 problem java

--- Set1_1000/239_Sliding_Window_Maximum.java
+++ Set1_1000/239_Sliding_Window_Maximum.java
@@ -16,3 +16,34 @@ class Solution {
         return result;
     }
 }
+
+
+// https://youtu.be/CZQGRp93K4k
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return new int []{};
+        }
+        int length = nums.length;
+        final int resultSize = length - k + 1;
+        int [] result = new int[resultSize];
+        // now we will be making use of double ended queue deque
+        Deque<Integer> queue = new ArrayDeque<>();
+        int resultIndex = 0;
+        for (int i=0; i<length; i++) {
+            // we need to maintain the sliding window of size k
+            if (!queue.isEmpty() && i - k == queue.peek()) {
+                queue.poll();
+            }
+            // remove all the smaller element from end as we need to maintain a decreasing queue
+            while (!queue.isEmpty() && nums[queue.peekLast()] < nums[i]) {
+                queue.pollLast();
+            }
+            queue.offer(i);
+            if (i >= k - 1) {
+                result[resultIndex++] = nums[queue.peek()];
+            }
+        }
+        return result;
+    }
+}
