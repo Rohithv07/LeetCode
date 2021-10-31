ef6be0c0bb7b351908aaaa09b650581d271dd72f
Rohithv07 <rohithv63@gmail.com>
2021-10-31 17:15:18 +0530

    Leetcode 337 problem java

--- Set1_1000/337_House_Robber_III.java
+++ Set1_1000/337_House_Robber_III.java
@@ -34,3 +34,45 @@ class Solution {
 }
 
 // refer : https://leetcode.com/problems/house-robber-iii/discuss/79330/Step-by-step-tackling-of-the-problem
+
+
+// same problem way using video solving
+
+// https://www.youtube.com/watch?v=nHR8ytpzz7c
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int [] withAndWithoutRoot = postOrder(root);
+        return Math.max(withAndWithoutRoot[0], withAndWithoutRoot[1]);
+    }
+    
+    // [withroot, withoutroot]
+    public int [] postOrder(TreeNode node) {
+        if (node == null) {
+            return new int []{0, 0};
+        }
+        int [] leftChild = postOrder(node.left);
+        int [] rightChild = postOrder(node.right);
+        int withRoot = node.val + leftChild[1] + rightChild[1];
+        int withOutRoot = Math.max(leftChild[0], leftChild[1]) + Math.max(rightChild[0], rightChild[1]);
+        return new int []{withRoot, withOutRoot};
+    }
+}
