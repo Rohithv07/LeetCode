50e62562ad7a11719854cffd79e403d2f81bfc6a
Rohithv07 <rohithv63@gmail.com>
2021-11-03 00:06:02 +0530

    Leetcode 516 problem java

--- Set1_1000/516_Longest_Palindromic_SubSequence.java
+++ Set1_1000/516_Longest_Palindromic_SubSequence.java
@@ -20,3 +20,31 @@ class Solution {
         return dp[0][n-1];
     }
 }
+
+
+// same stratergy slightly diff code
+
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        if (s.length() == 1) {
+            return 1;
+        }
+        int length = s.length();
+        int [][] dp = new int [length][length];
+        for (int i=length-1; i>=0; i--) {
+            dp[i][i] = 1;
+            for (int j=i+1; j<length; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                }
+                else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][length - 1];
+    }
+}
