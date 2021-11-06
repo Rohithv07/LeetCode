bf924122b3fbd4152d638e66affa9d9bc1dbd00f
Rohithv07 <rohithv63@gmail.com>
2021-11-06 19:44:38 +0530

    Leetcode 23 problem java

--- Set1_1000/23_Merge_K_Sorted_LIsts.java
+++ Set1_1000/23_Merge_K_Sorted_LIsts.java
@@ -31,31 +31,49 @@ class Solution {
 
 
 
-// divide and conquer
-public static ListNode mergeKLists(ListNode[] lists){
-    return partion(lists,0,lists.length-1);
-}
-
-public static ListNode partion(ListNode[] lists,int s,int e){
-    if(s==e)  return lists[s];
-    if(s<e){
-        int q=(s+e)/2;
-        ListNode l1=partion(lists,s,q);
-        ListNode l2=partion(lists,q+1,e);
-        return merge(l1,l2);
-    }else
-        return null;
-}
-
-//This function is from Merge Two Sorted Lists.
-public static ListNode merge(ListNode l1,ListNode l2){
-    if(l1==null) return l2;
-    if(l2==null) return l1;
-    if(l1.val<l2.val){
-        l1.next=merge(l1.next,l2);
-        return l1;
-    }else{
-        l2.next=merge(l1,l2.next);
-        return l2;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        return partition(lists, 0, lists.length - 1);
+    }
+    
+    public ListNode partition(ListNode [] lists, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        if (start == end) {
+            return lists[start];
+        }
+        int middle = start + (end - start) / 2;
+        ListNode part1 = partition(lists, start, middle);
+        ListNode part2 = partition(lists, middle + 1, end);
+        return merge(part1, part2);
+    }
+    
+    public ListNode merge(ListNode part1, ListNode part2) {
+        if (part1 == null) {
+            return part2;
+        }
+        if (part2 == null) {
+            return part1;
+        }
+        if (part1.val < part2.val) {
+            part1.next = merge(part1.next, part2);
+            return part1;
+        }
+        part2.next = merge(part1, part2.next);
+        return part2;
     }
 }
