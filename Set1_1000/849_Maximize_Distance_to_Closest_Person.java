63ec4c607cd3ca7cfb50fceb21be2dc644026875
Rohithv07 <rohithv63@gmail.com>
2021-11-10 16:07:09 +0530

    Leetcode 849 problem java

--- Set1_1000/849_Maximize_Distance_to_Closest_Person.java
+++ Set1_1000/849_Maximize_Distance_to_Closest_Person.java
@@ -40,3 +40,43 @@ class Solution {
         return answer;
     }
 }
+
+
+// first approach that will come to our mind
+
+
+class Solution {
+    public int maxDistToClosest(int[] seats) {
+        if (seats == null || seats.length == 0) {
+            return 0;
+        }
+        int length = seats.length;
+        int [] leftToRight = new int [length];
+        int [] rightToLeft = new int [length];
+        Arrays.fill(leftToRight, length);
+        Arrays.fill(rightToLeft, length);
+        int answer = 0;
+        for (int i=0; i<length; i++) {
+            if (seats[i] == 1) {
+                leftToRight[i] = 0;
+            }
+            else if (i > 0) {
+                leftToRight[i] = 1 + leftToRight[i - 1];
+            }
+        }
+        for (int i=length-1; i>=0; i--) {
+            if (seats[i] == 1) {
+                rightToLeft[i] = 0;
+            }
+            else if (i < length - 1) {
+                rightToLeft[i] = 1 + rightToLeft[i + 1];
+            }
+        }
+        for (int i=0; i<length; i++) {
+            if (seats[i] == 0) {
+                answer = Math.max(answer, Math.min(leftToRight[i], rightToLeft[i]));
+            }
+        }
+        return answer;
+    }
+}
