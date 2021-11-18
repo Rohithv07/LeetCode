030c560b01f0c67848abc3008e00975bd271966c
Rohithv07 <rohithv63@gmail.com>
2021-11-18 18:03:16 +0530

    Leetcode 25 problem java

--- Set1_1000/25_Reverse_Nodes_In_K_Group.java
+++ Set1_1000/25_Reverse_Nodes_In_K_Group.java
@@ -8,6 +8,57 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// https://youtu.be/Of0HPkk3JgI
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || k == 1) {
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = dummy;
+        ListNode pre = dummy;
+        ListNode nex = dummy;
+        int count = 0;
+        while (cur.next != null) {
+            count++;
+            cur = cur.next;
+        }
+        while (count >= k) {
+            cur = pre.next;
+            nex = cur.next;
+            for (int i=1; i<k; i++) {
+                cur.next = nex.next;
+                nex.next = pre.next;
+                pre.next = nex;
+                nex = cur.next;
+            }
+            pre = cur;
+            count -= k;
+        }
+        return dummy.next;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
         if (head == null)
@@ -54,3 +105,6 @@ class Solution {
         return previous;
     }
 }
+
+
+
