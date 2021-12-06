5d66a5a8dd487af574ef97db2dbd3e7ef850495f
Rohithv07 <rohithv63@gmail.com>
2021-12-06 23:48:32 +0530

    Leetcode 37 problem java

--- Set1_1000/37_Sudoku_Solver.java
+++ Set1_1000/37_Sudoku_Solver.java
@@ -1,3 +1,70 @@
+
+
+// more understandable and explanation : https://youtu.be/FWAIf_EVUKE
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+    
+    private boolean solve(char [][] board) {
+        for (int i=0; i<9; i++) {
+            for (int j=0; j<9; j++) {
+                if (board[i][j] == '.') {
+                    for (char ch = '1'; ch <= '9'; ch++) {
+                        if (isPossible(board, i, j, ch)) {
+                            board[i][j] = ch;
+                            if (solve(board)) {
+                                return true;
+                            }
+                            else {
+                                board[i][j] = '.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    private boolean isPossible(char [][] board, int row, int col, char number) {
+        for (int i=0; i<9; i++) {
+            if (board[row][i] == number) {
+                return false;
+            }
+            if (board[i][col] == number) {
+                return false;
+            }
+            if (board[3 * (row / 3) + (i / 3)][3 * (col / 3) + (i % 3)] == number) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
     private static final char EMPTY = '.';
     public void solveSudoku(char[][] board) {
