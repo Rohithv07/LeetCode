1d0ca2ca95a8f92dbe7eb416a7a953fcdacb6479
Rohithv07 <rohithv63@gmail.com>
2021-12-08 11:59:30 +0530

    Leetcode 51 problem java

--- Set1_1000/51_N-Queens.java
+++ Set1_1000/51_N-Queens.java
@@ -1,3 +1,87 @@
+// ref video : https://youtu.be/i05Ju7AftcM
+
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        char [][] board = new char[n][n];
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        backtrack(result, board, n, 0);
+        return result;
+    }
+    
+    private void backtrack(List<List<String>> result, char [][] board, int n, int pos) {
+        if (pos == n) {
+            List<String> current = new ArrayList<>();
+            for (char [] row : board) {
+                String currentRow = new String(row);
+                current.add(currentRow);
+            }
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        // check from every row current col which is pos
+        for (int i=0; i<n; i++) {
+            if (canWePlaceQueen(board, i, pos, n)) {
+                board[i][pos] = 'Q';
+                backtrack(result, board, n, pos + 1);
+                board[i][pos] = '.';
+            }
+        }
+    }
+    
+    private boolean canWePlaceQueen(char [][] board, int row, int col, int n) {
+        int copyRow = row;
+        int copyCol = col;
+        // left upper diagonal check for queen
+        while (copyRow >= 0 && copyCol >= 0) {
+            if (board[copyRow][copyCol] == 'Q') {
+                return false;
+            }
+            copyRow--;
+            copyCol--;
+        }
+        // left part of the current row check for queen
+        copyRow = row;
+        copyCol = col;
+        while (copyCol >= 0) {
+            if (board[copyRow][copyCol] == 'Q') {
+                return false;
+            }
+            copyCol--;
+        }
+        // left down diagonal check for queen
+        copyRow = row;
+        copyCol = col;
+        while (copyRow < n && copyCol >= 0) {
+            if (board[copyRow][copyCol] == 'Q') {
+                return false;
+            }
+            copyRow++;
+            copyCol--;
+        }
+        return true;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> result = new ArrayList<>();
