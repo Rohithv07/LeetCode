e479df0e9e933f905ac3d11005d32620b1b351f8
Rohithv07 <rohithv63@gmail.com>
2021-12-09 23:34:32 +0530

    Leetcode 875 problem java

--- Set1_1000/875_KOKO_Eating_Bananas.java
+++ Set1_1000/875_KOKO_Eating_Bananas.java
@@ -1,3 +1,51 @@
+// ref video see allocate pages video in youtube
+
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        if (piles == null || piles.length == 0 || h < 0) {
+            return -1;
+        }
+        int low = 1;
+        int high = findMax(piles);
+        return binarySearch(piles, h, low, high);
+    }
+    
+    public int binarySearch(int [] piles, int h, int low, int high) {
+        while (low <= high) {
+            int middle = low + (high - low) / 2;
+            if (kokoCanEat(piles, middle, h)) {
+                high = middle - 1;
+            }
+            else {
+                low = middle + 1;
+            }
+        }
+        return low;
+    }
+    
+    public boolean kokoCanEat(int [] piles, int banana, int h) {
+        int timeTaken = 0;
+        for (int pile : piles) {
+            timeTaken += (pile - 1) / banana + 1;
+        }
+        return timeTaken <= h;
+    }
+    
+    public int findMax(int [] piles) {
+        int max = -1;
+        for (int number : piles) {
+            if (number > max) {
+                max = number;
+            }
+        }
+        return max;
+    }
+}
+
+
+
+
+
 class Solution {
     public int minEatingSpeed(int[] piles, int H) {
         int right = Arrays.stream(piles).max().getAsInt();
