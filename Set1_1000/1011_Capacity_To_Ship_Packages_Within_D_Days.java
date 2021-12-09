273143944b69261561afd4c3c26d6f01bdab3557
Rohithv07 <rohithv63@gmail.com>
2021-12-09 20:23:22 +0530

    Leetcode 1011 problem java

--- Set1_1000/1011_Capacity_To_Ship_Packages_Within_D_Days.java
+++ Set1_1000/1011_Capacity_To_Ship_Packages_Within_D_Days.java
@@ -1,3 +1,73 @@
+// after understanding through the video for allocate pages : https://youtu.be/gYmWHvRHu-s
+
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        if (weights == null || weights.length == 0 || days < 0) {
+            return -1;
+        }
+        int low = findMax(weights);
+        int high = totalSum(weights);
+        return binarySearch(weights, days, low, high);
+    }
+    
+    public int binarySearch(int [] weights, int days, int low, int high) {
+        while (low <= high) {
+            int middle = low + (high - low) / 2;
+            if (fitsInGivenWeight(weights, middle, days)) {
+                high = middle - 1;
+            }
+            else {
+                low = middle + 1;
+            }
+        }
+        return low;
+    }
+    
+    public boolean fitsInGivenWeight(int [] weights, int barrier, int days) {
+        int daysTook = 1;
+        int weightInADay = 0;
+        for (int number : weights) {
+            weightInADay += number;
+            if (weightInADay > barrier) {
+                daysTook++;
+                weightInADay = number;
+            }
+            if (daysTook > days) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public int findMax(int [] weights) {
+        int max = -1;
+        for (int number : weights) {
+            if (number > max) {
+                max = number;
+            }
+        }
+        return max;
+    }
+    
+    public int totalSum(int [] weights) {
+        int sum = 0;
+        for (int number : weights) {
+            sum += number;
+        }
+        return sum;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
     public int shipWithinDays(int[] weights, int D) {
         int left = Arrays.stream(weights).max().getAsInt();
