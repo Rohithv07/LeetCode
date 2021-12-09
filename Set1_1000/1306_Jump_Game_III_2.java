adfb3a3bc12e934fb4a93f4e286a73b4cee37d7a
Rohithv07 <rohithv63@gmail.com>
2021-12-09 12:18:19 +0530

    Leetcode 1306 problem java

--- Set1_1000/1306_Jump_Game_III_2.java
+++ Set1_1000/1306_Jump_Game_III_2.java
@@ -21,3 +21,27 @@ class Solution {
         return false;
     }
 }
+
+// using dfs
+
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        if (arr == null || arr.length == 0) {
+            return false;
+        }
+        int length = arr.length;
+        boolean [] visited = new boolean [length];
+        return dfs(arr, start, length, visited);
+    }
+    
+    private boolean dfs(int [] arr, int position, int length, boolean [] visited) {
+        if (position < 0 || position >= length || visited[position]) {
+            return false;
+        }
+        if (arr[position] == 0) {
+            return true;
+        }
+        visited[position] = true;
+        return dfs(arr, position + arr[position], length, visited) || dfs(arr, position - arr[position], length, visited);
+    }
+}
