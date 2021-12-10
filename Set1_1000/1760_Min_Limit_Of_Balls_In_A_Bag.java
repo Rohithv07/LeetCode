b3030b32681a2c52aac685f17609e769bf1a158a
Rohithv07 <rohithv63@gmail.com>
2021-12-10 12:53:57 +0530

    Leetcode 1760 problem java

--- Set1_1000/1760_Min_Limit_Of_Balls_In_A_Bag.java
+++ Set1_1000/1760_Min_Limit_Of_Balls_In_A_Bag.java
@@ -1,3 +1,52 @@
+// same as allocate pages, aggressive cow video as reference :
+
+class Solution {
+    public int minimumSize(int[] nums, int maxOperations) {
+        if (nums == null || nums.length == 0 || maxOperations < 0) {
+            return -1;
+        }
+        int low = 1;
+        int high = findMax(nums);
+        return binarySearch(nums, low, high, maxOperations);
+    }
+    
+    public int binarySearch(int [] nums, int low, int high, int maxOperations) {
+        while (low <= high) {
+            int middle = low + (high - low) / 2;
+            if (canDivide(nums, middle, maxOperations)) {
+                high = middle - 1;
+            }
+            else {
+                low = middle + 1;
+            }
+        }
+        return low;
+    }
+    
+    public boolean canDivide(int [] nums, int barrier, int maxOperations) {
+        int count = 0;
+        for (int number : nums) {
+            count += (number - 1) / barrier;
+            if (count > maxOperations) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public int findMax(int [] nums) {
+        int max = -1;
+        for (int number : nums) {
+            if (number > max) {
+                max = number;
+            }
+        }
+        return max;
+    }
+}
+
+
+
 class Solution {
     public int minimumSize(int[] nums, int maxOperations) {
         int left = 1;
