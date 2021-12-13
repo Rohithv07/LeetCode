3e95746906d72aadbd17b303d46bbac8aadc41af
Rohithv07 <rohithv63@gmail.com>
2021-12-14 00:01:11 +0530

    Leetcode 14 problem java

--- Set1_1000/14_Longest_Common_Prefix.java
+++ Set1_1000/14_Longest_Common_Prefix.java
@@ -11,3 +11,28 @@ class Solution {
         return prefix;
     }
 }
+
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        Arrays.sort(strs);
+        String smallerString = strs[0];
+        String largerString = strs[strs.length - 1];
+        int i = 0;
+        int j = 0;
+        while (i < smallerString.length() && j < largerString.length()) {
+            if (smallerString.charAt(i) == largerString.charAt(j)) {
+                sb.append(smallerString.charAt(i));
+            }
+            else {
+                return sb.toString();
+            }
+            i++; j++;
+        }
+        return sb.toString();
+    }
+}
