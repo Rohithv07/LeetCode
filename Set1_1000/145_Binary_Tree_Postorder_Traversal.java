0b625ca14e330cfe19f9487d7a605294ae27e48f
Rohithv07 <rohithv63@gmail.com>
2021-12-15 19:55:06 +0530

    Leetcode 145 problem java

---
+++ Set1_1000/145_Binary_Tree_Postorder_Traversal.java
@@ -0,0 +1,47 @@
+
+// using 2 stack
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+        stack1.push(root);
+        while (!stack1.isEmpty()) {
+            TreeNode current = stack1.pop();
+            stack2.push(current);
+            if (current.left != null) {
+                stack1.push(current.left);
+            }
+            if (current.right != null) {
+                stack1.push(current.right);
+            }
+        }
+        while (!stack2.isEmpty()) {
+            result.add(stack2.pop().val);
+        }
+        return result;
+    }
+}
+
+
+// using 1 stack
+
