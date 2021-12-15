6ddddfcb241f5caece1146df060ebdf7071f3e49
Rohithv07 <rohithv63@gmail.com>
2021-12-15 19:24:37 +0530

    Leetcode 94 problem java

--- Set1_1000/94_BST_Inorder_Traversal.java
+++ Set1_1000/94_BST_Inorder_Traversal.java
@@ -33,3 +33,45 @@ class Solution {
         return result;
     }
 }
+
+// https://youtu.be/lxTGsVXjwvM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        while (true) {
+            if (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+            else {
+                if (stack.isEmpty()) {
+                    break;
+                }
+                current = stack.pop();
+                result.add(current.val);
+                current = current.right;
+            }
+        }
+        return result;
+    }
+}
