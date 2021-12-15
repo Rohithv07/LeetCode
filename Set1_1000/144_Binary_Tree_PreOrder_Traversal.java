5d025ec1389468018791b126a75725ffd10ba3d6
Rohithv07 <rohithv63@gmail.com>
2021-12-15 19:25:44 +0530

    Leetcode 144 problem java

--- Set1_1000/144_Binary_Tree_PreOrder_Traversal.java
+++ Set1_1000/144_Binary_Tree_PreOrder_Traversal.java
@@ -31,3 +31,41 @@ class Solution {
         return result;
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode current = stack.pop();
+            result.add(current.val);
+            if (current.right != null) {
+                stack.push(current.right);
+            }
+            if (current.left != null) {
+                stack.push(current.left);
+            }
+        }
+        return result;
+    }
+}
