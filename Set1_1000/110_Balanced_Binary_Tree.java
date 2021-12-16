ba0762975e52d7fd451571b2dee58cd06fb8a2e7
Rohithv07 <rohithv63@gmail.com>
2021-12-16 16:55:21 +0530

    Leetcode 110 problem java

--- Set1_1000/110_Balanced_Binary_Tree.java
+++ Set1_1000/110_Balanced_Binary_Tree.java
@@ -30,3 +30,44 @@ class Solution {
         return 1 + Math.max(height(node.left), height(node.right));
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        boolean [] result = new boolean [1];
+        result[0] = true;
+        dfs(root, result);
+        return result[0];
+    }
+    
+    public int dfs(TreeNode node, boolean [] result) {
+        if (node == null) {
+            return 0;
+        }
+        int left = dfs(node.left, result);
+        int right = dfs(node.right, result);
+        if (Math.abs(left - right) > 1) {
+            result[0] = false;
+            return -1;// some value to exit the check
+        }
+        return 1 + Math.max(left, right);
+    }
+}
