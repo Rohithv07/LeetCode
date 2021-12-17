c90e61f890538ce7c8f4a178f755631107f33697
Rohithv07 <rohithv63@gmail.com>
2021-12-17 15:43:31 +0530

    Leetcode 116 problem java

--- Set1_1000/116_Populating_Next_Right_Pointers_In_Each_Node.java
+++ Set1_1000/116_Populating_Next_Right_Pointers_In_Each_Node.java
@@ -78,3 +78,64 @@ class Solution {
         return root;
     }
 }
+
+
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Node previous = null;
+        Node veryEndLevel = root;
+        Node nextEnd = null;
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            if (current.left != null) {
+                queue.offer(current.left);
+                nextEnd = current.left;
+            }
+            if (current.right != null) {
+                queue.offer(current.right);
+                nextEnd = current.right;
+            }
+            if (previous != null) {
+                previous.next = current;
+            }
+            if (veryEndLevel == current) {
+                current.next = null;
+                previous = null;
+                veryEndLevel = nextEnd;
+            }
+            else {
+                previous = current;
+            }
+        }
+        return root;
+    }
+}
