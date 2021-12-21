94143ba1b38772028ed91e516bbbbe6b52b9fae0
Rohithv07 <rohithv63@gmail.com>
2021-12-21 18:10:31 +0530

    Leetcode 53 problem java

--- Set1_1000/53_MaximumSubArray.java
+++ Set1_1000/53_MaximumSubArray.java
@@ -23,3 +23,34 @@ class Solution {
  
  * Now just return the maximum sum and we get our answer.
 */
+
+
+
+// another solution but same concept
+
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int length = nums.length;
+        int sum = 0;
+        int max = nums[0];
+        int startingIndex = 0;
+        int endingIndex = 0;
+        for (int i=0; i<length; i++) {
+            sum += nums[i];
+            if (sum > max) {
+                max = sum;
+                endingIndex = i;
+            }
+            if (sum < 0) {
+                sum = 0;
+                startingIndex = i + 1;
+            }
+        }
+        System.out.println(startingIndex + " " + endingIndex);
+        return max;
+    }
+}
