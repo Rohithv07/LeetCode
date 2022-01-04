783c1e93fe7bc79b3b4432ff7ab36eeab9b3d25d
Rohithv07 <rohithv63@gmail.com>
2022-01-04 15:45:12 +0530

    Leetcode 300 problem java

--- Set1_1000/300_Longest_Increasing_SubSequence.java
+++ Set1_1000/300_Longest_Increasing_SubSequence.java
@@ -50,6 +50,39 @@ class Solution {
 }
 
 
+// idea behind the binary search with an example
+
+/*
+Let's construct the idea from following example.
+
+Consider the example nums = [2, 6, 8, 3, 4, 5, 1], let's try to build the increasing subsequences starting with an empty one: sub1 = [].
+
+Let pick the first element, sub1 = [2].
+6 is greater than previous number, sub1 = [2, 6]
+8 is greater than previous number, sub1 = [2, 6, 8]
+3 is less than previous number, we can't extend the subsequence sub1, but we must keep 3 because in the future there may have the longest subsequence start with [2, 3], sub1 = [2, 6, 8], sub2 = [2, 3].
+With 4, we can't extend sub1, but we can extend sub2, so sub1 = [2, 6, 8], sub2 = [2, 3, 4].
+With 5, we can't extend sub1, but we can extend sub2, so sub1 = [2, 6, 8], sub2 = [2, 3, 4, 5].
+With 1, we can't extend neighter sub1 nor sub2, but we need to keep 1, so sub1 = [2, 6, 8], sub2 = [2, 3, 4, 5], sub3 = [1].
+Finally, length of longest increase subsequence = len(sub2) = 4.
+
+In the above steps, we need to keep different sub arrays (sub1, sub2..., subk) which causes poor performance. 
+But we notice that we can just keep one sub array, when new number x is not greater than the last element of the subsequence sub, 
+we do binary search to find the smallest element >= x in sub, and replace with number x.
+
+Let's run that example nums = [2, 6, 8, 3, 4, 5, 1] again:
+Let pick the first element, sub = [2].
+6 is greater than previous number, sub = [2, 6]
+8 is greater than previous number, sub = [2, 6, 8]
+3 is less than previous number, so we can't extend the subsequence sub. We need to find the smallest number >= 3 in sub, it's 6. Then we overwrite it, now sub = [2, 3, 8].
+4 is less than previous number, so we can't extend the subsequence sub. We overwrite 8 by 4, so sub = [2, 3, 4].
+5 is greater than previous number, sub = [2, 3, 4, 5].
+1 is less than previous number, so we can't extend the subsequence sub. We overwrite 2 by 1, so sub = [1, 3, 4, 5].
+Finally, length of longest increase subsequence = len(sub) = 4.
+*/
+
+
+
 class Solution {
     public int lengthOfLIS(int[] nums) {
         if (nums == null || nums.length == 0)
