4dcd317e198806f309b55329de22fdc366f38369
Rohithv07 <rohithv63@gmail.com>
2022-01-09 23:47:17 +0530

    Leetcode 705 problem java

--- Set1_1000/705_Design_HashSet.java
+++ Set1_1000/705_Design_HashSet.java
@@ -1,3 +1,78 @@
+// same concept as that of hashmap
+
+class MyHashSet {
+
+    private ListNode [] bucket;
+    
+    public MyHashSet() {
+        bucket = new ListNode[1000000];
+    }
+    
+    public void add(int key) {
+        int indexToAdd = findIndex(key);
+        ListNode previous = findPrevious(indexToAdd, key);
+        if (previous.next == null) {
+            previous.next = new ListNode(key);
+        }
+        else {
+            previous.next.key = key;
+        }
+    }
+    
+    public void remove(int key) {
+        int indexToRemove = findIndex(key);
+        ListNode previous = findPrevious(indexToRemove, key);
+        if (previous.next != null) {
+            previous.next = previous.next.next;
+        }
+    }
+    
+    public boolean contains(int key) {
+        int indexToContains = findIndex(key);
+        ListNode previous = findPrevious(indexToContains, key);
+        
+        if (previous.next == null) {
+            return false;
+        }
+        return true;
+    }
+    
+    public int findIndex(int key) {
+        return Integer.hashCode(key) % bucket.length; // hashcode & n - 1
+    }
+    
+    public ListNode findPrevious(int index, int key) {
+        if (bucket[index] == null) {
+            return bucket[index] = new ListNode(-1);
+        }
+        ListNode previous = bucket[index];
+        while (previous.next != null && previous.next.key != key) {
+            previous = previous.next;
+        }
+        return previous;
+    }
+}
+
+class ListNode {
+    int key;
+    ListNode next;
+    public ListNode(int key) {
+        this.key = key;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
+
+
+
+
+
 // https://www.youtube.com/watch?v=NrMaQL_4Npo&t=1107s
 
 class MyHashSet {
