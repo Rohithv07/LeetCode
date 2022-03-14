fdb3976073cd5db7e41e4d426f42148acdeb9673
Rohithv07 <rohithv63@gmail.com>
2022-03-14 12:24:48 +0530

    Leetcode 71 problem in java

--- Set1_1000/71_Simplify_Path.java
+++ Set1_1000/71_Simplify_Path.java
@@ -13,3 +13,31 @@ class Solution {
         return "/" + String.join("/", stack);
     }
 }
+
+// using array deque
+
+class Solution {
+    public String simplifyPath(String path) {
+        if (path == null || path.length() == 0) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        String [] split = path.split("/");
+        Deque<String> deque = new ArrayDeque<>();
+        for (String s : split) {
+            if (s.length() == 0 || s.equals(".")) {
+                continue;
+            }
+            if (s.equals("..")) {
+                deque.pollFirst();
+            }
+            else {
+                deque.offerFirst(s);
+            }
+        }
+        while (!deque.isEmpty()) {
+            sb.append("/").append(deque.pollLast());
+        }
+        return sb.length() == 0 ? "/" : sb.toString();
+    }
+}
