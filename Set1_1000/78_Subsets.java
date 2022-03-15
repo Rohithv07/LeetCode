2e65aed046b33162c0a9fe00f07a69dc7cad297e
Rohithv07 <rohithv63@gmail.com>
2022-03-16 00:28:11 +0530

    Leetcode 78 problem in java

--- Set1_1000/78_Subsets.java
+++ Set1_1000/78_Subsets.java
@@ -16,3 +16,26 @@ class Solution {
         }
     }
 }
+
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return new ArrayList<>();
+        }
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+        backtrackHelper(nums, result, current, 0);
+        return result;
+    }
+    
+    public void backtrackHelper(int [] nums, List<List<Integer>> result, List<Integer> current, int position) {
+        result.add(new ArrayList<>(current));
+        for (int i=position; i<nums.length; i++) {
+            current.add(nums[i]);
+            backtrackHelper(nums, result, current, i + 1);
+            current.remove(current.size() - 1);
+        }
+    }
+}
