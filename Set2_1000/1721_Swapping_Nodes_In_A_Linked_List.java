6724b9e9616bf060005ac7ebafb886386ec07d9f
Rohithv07 <rohithv63@gmail.com>
2022-04-04 22:50:26 +0530

    Leetcode 1721 problem in java

--- Set2_1000/1721_Swapping_Nodes_In_A_Linked_List.java
+++ Set2_1000/1721_Swapping_Nodes_In_A_Linked_List.java
@@ -36,3 +36,70 @@ class Solution {
         return head;
     }
 }
+
+// without changing the node value rather changing the node pointer
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        int length = findLength(head);
+        if (k > length) {
+            return head;
+        }
+        ListNode previousFront = null;
+        ListNode front = head;
+        ListNode previousBack = null;
+        ListNode back = head;
+        for (int i = 1; i < k; i++) {
+            previousFront = front;
+            front = front.next;
+        }
+        for (int i = 1; i < length - k + 1; i++) {
+            previousBack = back;
+            back = back.next;
+        }
+        if (previousBack != null) {
+            previousBack.next = front;
+        }
+        if (previousFront != null) {
+            previousFront.next = back;
+        }
+        ListNode temp = front.next;
+        front.next = back.next;
+        back.next = temp;
+        if (k == 1) {
+            head = back;
+        }
+        if (k == length) {
+            head = front;
+        }
+        return head;
+    }
+    
+    private int findLength(ListNode node) {
+        if (node == null) {
+            return 0;
+        }
+        if (node.next == null) {
+            return 1;
+        }
+        int length = 0;
+        while (node != null) {
+            node = node.next;
+            length++;
+        }
+        return length;
+    }
+}
