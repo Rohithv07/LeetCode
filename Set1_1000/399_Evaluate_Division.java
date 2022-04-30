1372ca20895631acd3a468be4d0e8527c161356c
Rohithv07 <rohithv63@gmail.com>
2022-04-30 10:55:38 +0530

    Leetcode 399 problem in java

--- Set1_1000/399_Evaluate_Division.java
+++ Set1_1000/399_Evaluate_Division.java
@@ -55,3 +55,52 @@ class Solution {
         return p;
     }
 }
+
+
+
+
+// simple dfs
+
+class Solution {
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        // dfs, find a path between node and multiply everything bw nodes
+        Map<String, Map<String, Double>> graph = buildGraph(equations, values);
+        double [] result = new double[queries.size()];
+        for (int i = 0; i < queries.size(); i++) {
+            result[i] = dfs(graph, queries.get(i).get(0), queries.get(i).get(1), new HashSet<>());
+        }
+        return result;
+    }
+    
+    private double dfs(Map<String, Map<String, Double>> graph, String q1, String q2, Set<String> visited) {
+        if (!graph.containsKey(q1)) {
+            return -1.0;
+        }
+        if (graph.get(q1).containsKey(q2)) {
+            return graph.get(q1).get(q2);
+        }
+        visited.add(q1);
+        for (Map.Entry<String, Double> children : graph.get(q1).entrySet()) {
+            if (!visited.contains(children.getKey())) {
+                double product = dfs(graph, children.getKey(), q2, visited);
+                if (product != -1.0) {
+                    return children.getValue() * product;
+                }
+            }
+        }
+        return -1.0;
+    }
+    
+    private Map<String, Map<String, Double>> buildGraph(List<List<String>> equations, double [] values) {
+        Map<String, Map<String, Double>> graph = new HashMap<>();
+        int index = 0;
+        for (List<String> equation : equations) {
+            String u = equation.get(0);
+            String v = equation.get(1);
+            double weight = values[index++];
+            graph.computeIfAbsent(u, x -> new HashMap<>()).put(v, weight);
+            graph.computeIfAbsent(v, x -> new HashMap<>()).put(u, 1 / weight);
+        }
+        return graph;
+    }
+}
